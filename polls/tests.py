c13a55731e4a9c7c948aae4a43e50889a757d611
Ian <43066864+IanC13@users.noreply.github.com>
2026-03-09 17:47:24 -0400

    Add dummy test that asserts False

--- polls/tests.py
+++ polls/tests.py
@@ -4,3 +4,7 @@ from django.test import TestCase
 class SmokeTest(TestCase):
     def test_sanity(self):
         self.assertTrue(True)
+
+    def test_dummy(self):
+        self.assertTrue(False)
+        
